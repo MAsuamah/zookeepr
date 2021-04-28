f3cb7fa7935f95e4fe964a6b90c3f28cc33769ce
Michelle <michey.asmah21@gmail.com>
2021-04-27 21:49:32 -0400

    front-end

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
-
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -80,7 +82,6 @@ function validateAnimal(animal) {
   return true;
 }
 
-
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -109,6 +110,22 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(3001, () => {
