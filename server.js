6b422289df6ad9c1b2041542bed7afbf9e476166
Michelle <michey.asmah21@gmail.com>
2021-04-29 16:44:49 -0400

    port variable change

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`API server now on port 3001!`);
 });
